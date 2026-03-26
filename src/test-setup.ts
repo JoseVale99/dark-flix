fbc550d9154c8a367396aef310a47f00ccc5f924
josezarate <valezarate.9903@gmail.com>
2026-03-26 16:12:03 -0600

    test(setup): initialize Angular testing environment
    - Import Angular compiler and testing modules
    - Configure TestBed with BrowserDynamicTestingModule
    - Initialize platformBrowserDynamicTesting for test execution
    - Enable Angular component and service testing capabilities

---
+++ src/test-setup.ts
@@ -0,0 +1,11 @@
+import '@angular/compiler';
+import {
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting,
+} from '@angular/platform-browser-dynamic/testing';
+import { TestBed } from '@angular/core/testing';
+
+TestBed.initTestEnvironment(
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting()
+);
