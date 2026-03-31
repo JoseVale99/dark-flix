aaf3f70a66c4a4ded60a9cc13a3e5bd8a3b580fa
josezarate <valezarate.9903@gmail.com>
2026-03-31 15:04:37 -0600

    feat: update WpMediaService to use environment-based API configuration and add project documentation and settings

--- src/app/core/services/wp-media.ts
+++ src/app/core/services/wp-media.ts
@@ -10,13 +10,13 @@ import { environment } from '@env';
 export class WpMediaService {
   private http = inject(HttpClient);
 
-  // Extraemos la base url desde el environment param parametrizado
-  private readonly baseUrl = environment.apiUrl;
+  // Extraemos la base url limpia
+  private readonly baseUrl = environment.apiBaseUrl;
 
   /**
    * Obtiene la primera página de posts del catálogo (con metadata embedida)
    */
   getMediaCatalog(): Observable<WpPost[]> {
-    return this.http.get<WpPost[]>(this.baseUrl);
+    return this.http.get<WpPost[]>(`${this.baseUrl}/posts?_embed`);
   }
 }
